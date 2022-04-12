99a1b692fabdbe0e81ca11b8f1a7d07abf3354bb
David Stojanovski <dgstojanovski@outlook.com>
2022-04-12 17:54:49 +1000

    Initial commit

---
+++ Organiser/Enumerations/Priority.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organiser.Enumerations
+{
+    public enum Priority
+    {
+        Lowest = 0,
+        Lower = 1,
+        Low = 2,
+        Medium = 3,
+        High = 4,
+        Higher = 5,
+        Highest = 6
+    }
+}
---
+++ Organiser/Models/Planning/CalendarTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Organiser.Enumerations;
+
+namespace Organiser.Models.Planning
+{
+    public class CalendarTask
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime? Deadline { get; set; }
+
+        public Priority Priority { get; set; }
+    }
+}
